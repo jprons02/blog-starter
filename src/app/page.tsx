3bdafe982aaddb1e8f5803088da796fb337ff706
jprons02 <joseph.ronselli@gmail.com>
2025-06-18 19:41:47 -0400

    added blogs back

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,6 @@
-//import { allPosts } from "contentlayer/generated";
-//import BlogIndexClient from "@/app/posts/BlogIndexClient";
+import { allPosts } from "contentlayer/generated";
+import BlogIndexClient from "@/app/posts/BlogIndexClient";
 
-/*
 export default function BlogIndexPage() {
   return <BlogIndexClient posts={allPosts} />;
-}
-*/
-
-export default function BlogIndexPage() {
-  return <div>test.</div>;
 }
