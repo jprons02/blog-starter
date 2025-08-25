4fabd480fea5068d130c5b6a67c9a4ee8c8f78fa
jprons02 <joseph.ronselli@gmail.com>
2025-08-25 18:48:18 -0400

    working hydration error only in prod

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const isDev = process.env.NODE_ENV === "development";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  productionBrowserSourceMaps: true,
   ...(isDev && {
     experimental: {
       allowDevelopmentBuild: true,
